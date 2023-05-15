a6f1c2cbca931833ecc89818e01f19c156341896
Afshin <fallahi_a@yahoo.com>
2023-05-15 16:06:20 -0400

    Routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,13 @@
+import {Routes , Route} from  "react-router-dom";
+import BookingPage from "../Pages/BookingPage";
+import HomePage from "../Pages/HomePage";
+
 function Main() {
     return (
-        <div></div>
+        <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/booking" element={<BookingPage />}></Route>
+        </Routes>
     );
 }
 
