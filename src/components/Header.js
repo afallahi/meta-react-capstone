e544a0eda148e9946b456457822810651635a6ab
Afshin <fallahi_a@yahoo.com>
2023-05-15 13:27:19 -0400

    add semantics and logo

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Logo from "../Logo.svg"
+
+function Header () {
+    return (
+        <div>
+            <img src={Logo} alt="logo"/>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return (
+        <div>
+          <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>MENU</li>
+            <li>RESERVATIONS</li>
+            <li>ORDER ONLINE</li>
+            <li>LOGIN</li>
+          </ul>
+        </div>
+    );
+}
+
+export default Nav;
